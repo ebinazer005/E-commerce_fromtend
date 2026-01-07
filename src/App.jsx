9650cd7e2247cff568c9b511268650ef3c624956
ebinazer005 <ebinazerselvaraj206@gmail.com>
2026-01-07 23:17:22 +0530

    change the backend url

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Router>
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/signinpage" element={<Signin />} />
+        <Route path="/" element={<Signin />} />
       </Routes>
     </Router>
 
